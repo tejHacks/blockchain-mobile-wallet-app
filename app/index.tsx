785f1f07bb967f2183adde8be243294121bcba57
tejHacks <olateju202@gmail.com>
2025-11-11 13:23:12 +0100

    first commit

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text className="text-4xl text-red-900">Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+  
